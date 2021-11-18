a5f5333ff2c4f827785afd7c0a9effd87135be86
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-11-18 19:43:09 +0330

    Added Patrick

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:whatsdone/app/index.dart';
 
 void main() => runApp(MyApp());
@@ -10,9 +11,8 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: WhatsDone(),
       theme: ThemeData(
-        // textTheme: GoogleFonts.boogalooTextTheme(),
-        // backgroundColor: Colors.white,
         primarySwatch: Colors.deepPurple,
+        textTheme: GoogleFonts.patrickHandTextTheme(),
       ),
     );
   }
