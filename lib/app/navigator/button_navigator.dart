e829cb3a22f7573a2f20d19c13a3c1f22fdfdf0f
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-11-18 19:20:34 +0330

    Create navigator

---
+++ lib/app/navigator/button_navigator.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class bNavigator extends StatelessWidget {
+  bNavigator({@required this.change, @required this.selected});
+  final change;
+  final selected;
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      elevation: 10,
+      onTap: change,
+      backgroundColor: Colors.grey[200],
+      currentIndex: selected,
+      selectedItemColor: Colors.deepPurple,
+      unselectedItemColor: Colors.grey,
+      items: <BottomNavigationBarItem>[
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+        BottomNavigationBarItem(icon: Icon(Icons.add), label: 'Add'),
+        BottomNavigationBarItem(icon: Icon(Icons.check), label: 'Done'),
+      ],
+    );
+  }
+}
