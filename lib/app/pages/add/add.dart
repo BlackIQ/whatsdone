f9ee1ef7c096d0c3a7f7706fe8cab2387022136a
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-11-18 21:32:25 +0330

    Adding form backend

--- lib/app/pages/add/add.dart
+++ lib/app/pages/add/add.dart
@@ -6,14 +6,55 @@ class AddTask extends StatefulWidget {
 }
 
 class _AddTaskState extends State<AddTask> {
+
+  String _task = '';
+
+  void changeText(String task) {
+    setState(() {
+      _task = task;
+    });
+  }
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       padding: EdgeInsets.all(20),
-      child: Column(
-        children: <Widget>[
-          Text('Add'),
-        ],
+      child: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          // mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Task name',
+              ),
+              validator: (String? value) {
+                if (value!.isEmpty) {
+                  return 'Task name is required';
+                }
+              },
+              onSaved: (String? value) {
+                changeText(value!);
+              },
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                if (!_formKey.currentState!.validate()) {
+                  changeText('');
+                }
+                _formKey.currentState!.save();
+                print(_task);
+              },
+              child: Text(
+                'Add this task'
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
