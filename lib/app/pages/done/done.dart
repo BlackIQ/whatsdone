b54bb787eaf8b1d30fa577d51c8bf303381b5fde
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-11-19 01:41:38 +0330

    Added with timer and message

--- lib/app/pages/done/done.dart
+++ lib/app/pages/done/done.dart
@@ -6,15 +6,84 @@ class DoneTasks extends StatefulWidget {
 }
 
 class _DoneTasksState extends State<DoneTasks> {
+  final List<String> itemsList = [
+    'A',
+    'B',
+    'C',
+    'D',
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      padding: EdgeInsets.all(20),
-      child: Column(
-        children: <Widget>[
-          Text('Done'),
-        ],
-      ),
-    );
+    return itemsList.length > 0
+        ? ListView.separated(
+            itemCount: itemsList.length,
+            itemBuilder: (context, index) {
+              return Dismissible(
+                key: UniqueKey(),
+                child: ListTile(
+                  title: Container(
+                    // padding: EdgeInsets.all(5),
+                    margin: EdgeInsets.all(5),
+                    // color: Colors.grey[200],
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text('${itemsList[index]}'),
+                        SizedBox(height: 5),
+                        Text(
+                          'Date',
+                          style:
+                              TextStyle(fontSize: 12, color: Colors.deepPurple),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                onDismissed: (direction) {
+                  setState(() {
+                    itemsList.removeAt(index);
+                  });
+                },
+                background: Container(
+                  color: Colors.blueAccent,
+                  child: Icon(
+                    Icons.timer,
+                    color: Colors.white,
+                  ),
+                ),
+                secondaryBackground: Container(
+                  color: Colors.red,
+                  child: Icon(
+                    Icons.cancel,
+                    color: Colors.white,
+                  ),
+                ),
+              );
+            },
+            separatorBuilder: (BuildContext context, int index) =>
+                const Divider(color: Colors.deepPurple),
+          )
+        : Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'You did everything',
+                  style: TextStyle(
+                    fontSize: 35,
+                    color: Colors.green,
+                  ),
+                ),
+                Text(
+                  'congratulations',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.deepPurple,
+                  ),
+                ),
+              ],
+            ),
+          );
   }
 }
