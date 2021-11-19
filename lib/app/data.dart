0886e853f2c6e554cd65449c061e3d9ca7115016
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-11-20 00:01:24 +0330

    Add data file to create sqflites things

---
+++ lib/app/data.dart
@@ -0,0 +1,22 @@
+// "name": "In Home",
+// "date": "2021 Nov 1",
+// "status": "home",
+// "id": 1
+
+
+
+void done(int row) {
+  // allItems[row]['status'] = 'done';
+}
+
+void trash(int row) {
+  // allItems[row]['status'] = 'trash';
+}
+
+void home(int row) {
+  // allItems[row]['status'] = 'home';
+}
+
+void delete(int row) {
+  // allItems[row]['status'] = 'deleted';
+}
