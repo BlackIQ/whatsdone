525684617b7098fd90a9bde7941640e787508f37
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-11-19 12:09:00 +0330

    Adding some pages for developer and trash

---
+++ lib/app/routes/developer.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Developer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Developer Page'),
+    );
+  }
+}
---
+++ lib/app/routes/trash.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Trash extends StatefulWidget {
+  @override
+  _TrashState createState() => _TrashState();
+}
+
+class _TrashState extends State<Trash> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Trash page'),
+    );
+  }
+}
