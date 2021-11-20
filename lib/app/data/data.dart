ba6d15623d83514a6d2bb6c8c242150325a16a0d
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-11-20 11:41:46 +0330

    Added text, date and id

--- lib/app/data/data.dart
+++ lib/app/data/data.dart
@@ -1,3 +1,16 @@
+import 'dart:math';
+import 'package:intl/intl.dart';
+
+void insert(String task) {
+  String _task = task;
+  var ontime = DateFormat('y/MMM/dd H:m:s').format(DateTime.now());
+  int id = Random().nextInt(999);
+
+  print(task);
+  print(id);
+  print(ontime);
+}
+
 void done(int row) {
   // Done
 }
