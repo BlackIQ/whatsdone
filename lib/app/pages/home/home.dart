08c8bdb87a266e07d54af0037c7d919e731060f7
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-11-28 00:55:43 +0330

    Update fucking page

--- lib/app/pages/home/home.dart
+++ lib/app/pages/home/home.dart
@@ -10,7 +10,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<List<Tasks>>(
-      future: DatabaseHelper.instance.getTasks('home'),
+      future: DatabaseHelper.instance.getHome(),
       builder: (BuildContext context, AsyncSnapshot<List<Tasks>> snapshot) {
         if (!snapshot.hasData) {
           return Center(
@@ -44,101 +44,52 @@ class _HomeState extends State<Home> {
             : ListView(
                 children: snapshot.data!.map((task) {
                   return Center(
-                    child: ListTile(
-                      trailing: FlatButton(
+                    child: Dismissible(
+                      key: UniqueKey(),
+                      child: ListTile(
+                        title: Container(
+                          margin: EdgeInsets.all(5),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(task.name!),
+                              SizedBox(height: 5),
+                              Text(
+                                task.date!,
+                                style: TextStyle(fontSize: 12, color: Colors.deepPurple),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      onDismissed: (direction) async {
+                        if (direction == DismissDirection.endToStart) {
+                          // TODO: Insert in trash
+                          // TODO: Delete in home
+                        } else {
+                          // TODO: Insert in done
+                          // TODO: Delete in home
+                        }
+                      },
+                      background: Container(
+                        color: Colors.green,
+                        child: Icon(
+                          Icons.check,
+                          color: Colors.white,
+                        ),
+                      ),
+                      secondaryBackground: Container(
+                        color: Colors.red,
                         child: Icon(
                           Icons.delete,
-                          color: Colors.red,
+                          color: Colors.white,
                         ),
-                        onPressed: () {
-                          setState(() {
-                            DatabaseHelper.instance.(task.id!);
-                          });
-                        },
                       ),
-                      title: Text(task.name.toString()),
-                      subtitle: Text(task.date!),
                     ),
                   );
                 }).toList(),
               );
       },
     );
-  }
-
-  Widget _page(List itemsList) {
-    return itemsList.length != 0
-        ? ListView.separated(
-            itemCount: itemsList.length,
-            itemBuilder: (context, index) {
-              return Dismissible(
-                key: UniqueKey(),
-                child: ListTile(
-                  title: Container(
-                    margin: EdgeInsets.all(5),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text('${itemsList[index]['name']}'),
-                        SizedBox(height: 5),
-                        Text(
-                          '${itemsList[index]['date']}',
-                          style:
-                              TextStyle(fontSize: 12, color: Colors.deepPurple),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                onDismissed: (direction) {
-                  if (direction == DismissDirection.endToStart) {
-                    // trash(index);
-                    setState(() {
-                      itemsList.removeAt(index);
-                    });
-                  } else {
-                    // done(index);
-                  }
-                },
-                background: Container(
-                  color: Colors.green,
-                  child: Icon(
-                    Icons.check,
-                    color: Colors.white,
-                  ),
-                ),
-                secondaryBackground: Container(
-                  color: Colors.red,
-                  child: Icon(
-                    Icons.delete,
-                    color: Colors.white,
-                  ),
-                ),
-              );
-            },
-            separatorBuilder: (BuildContext context, int index) =>
-                Divider(color: Colors.deepPurple),
-          )
-        : Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'Nothing added',
-                  style: TextStyle(
-                    fontSize: 35,
-                    color: Colors.deepPurple,
-                  ),
-                ),
-                Text(
-                  'List is empty',
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.grey,
-                  ),
-                ),
-              ],
-            ),
-          );
   }
 }
