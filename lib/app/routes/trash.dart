60d3b6f8679756fbcd35a87fc960f9f60fead152
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-11-28 01:08:09 +0330

    Update trash template

--- lib/app/routes/trash.dart
+++ lib/app/routes/trash.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:whatsdone/app/data/data.dart';
 
 class Trash extends StatefulWidget {
   @override
@@ -7,11 +8,6 @@ class Trash extends StatefulWidget {
 }
 
 class _TrashState extends State<Trash> {
-  List<Map> itemsList = [
-    {'name': 'Issues in Trash', 'date': 'Nov 15 2021'},
-    {'name': 'Watch HIMYM', 'date': 'Nov 21 2021'},
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,76 +19,88 @@ class _TrashState extends State<Trash> {
           style: GoogleFonts.patrickHand(),
         ),
       ),
-      body: itemsList.length > 0
-          ? ListView.separated(
-        itemCount: itemsList.length,
-        itemBuilder: (context, index) {
-          return Dismissible(
-            key: UniqueKey(),
-            child: ListTile(
-              title: Container(
-                margin: EdgeInsets.all(5),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text('${itemsList[index]['name']}'),
-                    SizedBox(height: 5),
-                    Text(
-                      '${itemsList[index]['date']}',
-                      style: TextStyle(fontSize: 12, color: Colors.red),
-                    ),
-                  ],
-                ),
+      body: FutureBuilder<List<Tasks>>(
+        future: DatabaseHelper.instance.getTrash(),
+        builder: (BuildContext context, AsyncSnapshot<List<Tasks>> snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(
+                color: Colors.purple,
               ),
-            ),
-            onDismissed: (direction) {
-              if (direction == DismissDirection.endToStart) {
-                setState(() {
-                  itemsList.removeAt(index);
-                });
-              }
-              else {
-                // Un Done
-              }
-            },
-            background: Container(
-              color: Colors.blueAccent,
-              child: Icon(
-                Icons.timer,
-                color: Colors.white,
-              ),
-            ),
-            secondaryBackground: Container(
-              color: Colors.red,
-              child: Icon(
-                Icons.delete,
-                color: Colors.white,
-              ),
-            ),
-          );
+            );
+          }
+          return snapshot.data!.isEmpty
+              ? Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        'Nothing added',
+                        style: TextStyle(
+                          fontSize: 35,
+                          color: Colors.deepPurple,
+                        ),
+                      ),
+                      Text(
+                        'List is empty',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              : ListView(
+                  children: snapshot.data!.map((task) {
+                    return Center(
+                      child: Dismissible(
+                        key: UniqueKey(),
+                        child: ListTile(
+                          title: Container(
+                            margin: EdgeInsets.all(5),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(task.name!),
+                                SizedBox(height: 5),
+                                Text(
+                                  task.date!,
+                                  style: TextStyle(
+                                      fontSize: 12, color: Colors.deepPurple),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        onDismissed: (direction) async {
+                          if (direction == DismissDirection.endToStart) {
+                            // TODO: Insert in trash
+                            // TODO: Delete in home
+                          } else {
+                            // TODO: Insert in done
+                            // TODO: Delete in home
+                          }
+                        },
+                        background: Container(
+                          color: Colors.green,
+                          child: Icon(
+                            Icons.check,
+                            color: Colors.white,
+                          ),
+                        ),
+                        secondaryBackground: Container(
+                          color: Colors.red,
+                          child: Icon(
+                            Icons.delete,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    );
+                  }).toList(),
+                );
         },
-        separatorBuilder: (BuildContext context, int index) => Divider(color: Colors.red),
-      )
-          : Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Trash is empty',
-              style: TextStyle(
-                fontSize: 35,
-                color: Colors.red,
-              ),
-            ),
-            Text(
-              'Well done',
-              style: TextStyle(
-                fontSize: 20,
-                color: Colors.deepPurple,
-              ),
-            ),
-          ],
-        ),
       ),
     );
   }
