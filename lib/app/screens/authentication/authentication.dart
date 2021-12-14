1c0744753e1affaa4706cc15903fcb106015cb87
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-12-14 13:15:50 +0330

    Just create a simple file

---
+++ lib/app/screens/authentication/authentication.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Authentication extends StatefulWidget {
+  @override
+  _AuthenticationState createState() => _AuthenticationState();
+}
+
+class _AuthenticationState extends State<Authentication> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
