7021b476c20b5e5fbb42b0691ed5b2d56f2b1a26
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-12-14 13:48:30 +0330

    Create a sample list to fix issues

--- lib/app/screens/home/routes/trash.dart
+++ lib/app/screens/home/routes/trash.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:whatsdone/app/data/data.dart';
 
 class Trash extends StatefulWidget {
   @override
@@ -8,6 +7,13 @@ class Trash extends StatefulWidget {
 }
 
 class _TrashState extends State<Trash> {
+  List<Map> x = [
+    {
+      'name': 'amir',
+      'date': 'amir',
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,79 +26,41 @@ class _TrashState extends State<Trash> {
           style: GoogleFonts.patrickHand(),
         ),
       ),
-      body: FutureBuilder<List<Tasks>>(
-        future: DatabaseHelper.instance.getTrash(),
-        builder: (BuildContext context, AsyncSnapshot<List<Tasks>> snapshot) {
-          if (!snapshot.hasData) {
-            return Center(
-              child: CircularProgressIndicator(
-                color: Colors.red,
+      body: ListView.builder(
+        itemCount: x.length,
+        itemBuilder: (context, index) {
+          return Dismissible(
+            key: UniqueKey(),
+            child: ListTile(
+              title: Container(
+                margin: EdgeInsets.all(5),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(x[index]['name']),
+                    SizedBox(height: 5),
+                    Text(
+                      x[index]['date'],
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.red,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            );
-          }
-          return snapshot.data!.isEmpty
-              ? Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text(
-                        'Trash is empty',
-                        style: TextStyle(
-                          fontSize: 35,
-                          color: Colors.red,
-                        ),
-                      ),
-                      Text(
-                        'Well done',
-                        style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ],
-                  ),
-                )
-              : RefreshIndicator(
-                  color: Colors.red,
-                  onRefresh: pageRefresh,
-                  child: ListView(
-                    children: snapshot.data!.map((task) {
-                      return Dismissible(
-                        key: UniqueKey(),
-                        child: ListTile(
-                          title: Container(
-                            margin: EdgeInsets.all(5),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: <Widget>[
-                                Text(task.name!),
-                                SizedBox(height: 5),
-                                Text(
-                                  task.date!,
-                                  style: TextStyle(
-                                      fontSize: 12, color: Colors.red),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                        onDismissed: (direction) {
-                          setState(() {
-                            DatabaseHelper.instance
-                                .remove(task.id!, 'trash');
-                          });
-                        },
-                        background: Container(
-                          color: Colors.red,
-                          child: Icon(
-                            Icons.delete,
-                            color: Colors.white,
-                          ),
-                        ),
-                      );
-                    }).toList(),
-                  ),
-                );
+            ),
+            onDismissed: (direction) {
+              print('Delete');
+            },
+            background: Container(
+              color: Colors.red,
+              child: Icon(
+                Icons.delete,
+                color: Colors.white,
+              ),
+            ),
+          );
         },
       ),
     );
