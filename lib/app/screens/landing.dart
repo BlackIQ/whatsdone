48cbbdc80abd64f35cc796a3893762928cda2092
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-12-14 15:13:22 +0330

    Add user main method

--- lib/app/screens/landing.dart
+++ lib/app/screens/landing.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:whatsdone/app/models/user.dart';
 import 'package:whatsdone/app/screens/home/home.dart';
 import 'package:whatsdone/app/screens/authentication/authentication.dart';
 
 class Landing extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final user = 'null';
+    final user = Provider.of<User>(context);
     return user != null ? Home() : Authentication();
   }
 }
