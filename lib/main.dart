cee45aef72279fbfff577443f1396495cc07ab72
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-12-14 13:09:48 +0330

    Fix import name

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:whatsdone/app/screens/home/index.dart';
+import 'package:whatsdone/app/screens/home/home.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
