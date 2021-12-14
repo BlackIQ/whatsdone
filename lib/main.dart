ae05c3bd0f4e273e2eff74bd1572e02ce4593a0c
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-12-14 15:13:09 +0330

    Init provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'package:whatsdone/app/models/user.dart';
 import 'package:whatsdone/app/screens/landing.dart';
+import 'package:whatsdone/app/services/authenticarion.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Landing(),
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-        textTheme: GoogleFonts.patrickHandTextTheme(),
+    return StreamProvider<User>.value(
+      value: AuthService().user,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Landing(),
+        theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
+          textTheme: GoogleFonts.patrickHandTextTheme(),
+        ),
       ),
     );
   }
