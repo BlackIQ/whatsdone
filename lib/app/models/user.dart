f8ada36479c9e30204d155d6d479757eca79049c
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-12-14 15:07:34 +0330

    Add user model

---
+++ lib/app/models/user.dart
@@ -0,0 +1,15 @@
+class User {
+  User({
+    this.uid,
+    this.profile,
+    this.name,
+    this.email,
+    this.phone,
+  });
+
+  final uid;
+  final profile;
+  final name;
+  final email;
+  final phone;
+}
