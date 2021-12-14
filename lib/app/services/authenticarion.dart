8f645c067b86d4252f80b8d3fe89ae905d5ea1e7
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-12-14 15:07:58 +0330

    Create services file

---
+++ lib/app/services/authenticarion.dart
@@ -0,0 +1,55 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:whatsdone/app/models/user.dart';
+
+class AuthService {
+  // Init firebase
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn _google = GoogleSignIn();
+
+  // Convert firebase user
+  User _userFromFirebaseUser(FirebaseUser user) {
+    if (user != null) {
+      return User(
+        uid: user.uid,
+        profile: user.photoUrl,
+        name: user.displayName,
+        email: user.email,
+        phone: user.phoneNumber,
+      );
+    } else {
+      return null;
+    }
+  }
+
+  // User
+  Stream<User> get user {
+    return _auth.onAuthStateChanged.map(_userFromFirebaseUser);
+  }
+
+  // Signin with Google
+  Future googleLogin() async {
+    try {
+      GoogleSignInAccount googleAccount = await _google.signIn();
+      if (googleAccount != null) {
+        GoogleSignInAuthentication googleAuth =
+        await googleAccount.authentication;
+
+        if (googleAuth.idToken != null && googleAuth.accessToken != null) {
+          AuthResult result = await _auth.signInWithCredential(
+            GoogleAuthProvider.getCredential(
+              idToken: googleAuth.idToken,
+              accessToken: googleAuth.accessToken,
+            ),
+          );
+          return _userFromFirebaseUser(result.user);
+        }
+      }
+    } catch (e) {
+      return [
+        null,
+        e.toString(),
+      ];
+    }
+  }
+}
