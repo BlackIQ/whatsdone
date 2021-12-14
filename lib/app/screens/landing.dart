c892ca571cab7060c3ce09c41b5f2f629256740f
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-12-14 15:08:08 +0330

    Fix small issues

--- lib/app/screens/landing.dart
+++ lib/app/screens/landing.dart
@@ -3,8 +3,6 @@ import 'package:whatsdone/app/screens/home/home.dart';
 import 'package:whatsdone/app/screens/authentication/authentication.dart';
 
 class Landing extends StatelessWidget {
-  const Landing({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     final user = 'null';
