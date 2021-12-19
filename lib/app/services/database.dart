7c70a4be00f93e033f492f40a7ce341a32d15b5f
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-12-19 20:53:52 +0330

    Working

--- lib/app/services/database.dart
+++ lib/app/services/database.dart
@@ -2,41 +2,7 @@ import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 import 'dart:io';
-
-class Tasks {
-  Tasks({
-    this.row,
-    this.name,
-    this.id,
-    this.status,
-    this.date,
-  });
-
-  final int row;
-  final String name;
-  final int id;
-  final String status;
-  final String date;
-
-  factory Tasks.fromMap(Map<String, dynamic> json) {
-    return Tasks(
-      row: json['row'],
-      name: json['name'],
-      id: json['id'],
-      status: json['status'],
-      date: json['date'],
-    );
-  }
-
-  Map<String, dynamic> toMap() {
-    return {
-      'row': row,
-      'name': name,
-      'id': id,
-      'date': date,
-    };
-  }
-}
+import 'package:whatsdone/app/models/task.dart';
 
 class DatabaseHelper {
   DatabaseHelper._privateConstructor();
@@ -45,11 +11,11 @@ class DatabaseHelper {
 
   static Database _database;
 
-  Future<Database> get database async => _database ??= await _initDatabase();
+  Future<Database> get database async => _database = await _initDatabase();
 
   Future<Database> _initDatabase() async {
     Directory documentsDirectory = await getApplicationDocumentsDirectory();
-    String path = join(documentsDirectory.path, 'whatsdone.db');
+    String path = join(documentsDirectory.path, 'wd3.db');
     return await openDatabase(
       path,
       version: 1,
