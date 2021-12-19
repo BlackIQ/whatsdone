b28af0444db2f00b945227be01e43e2d5d8a7177
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-12-19 20:52:57 +0330

    Add import for task

--- lib/app/screens/home/routes/trash.dart
+++ lib/app/screens/home/routes/trash.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:whatsdone/app/services/database.dart';
+import 'package:whatsdone/app/models/task.dart';
 
 class Trash extends StatefulWidget {
   @override
