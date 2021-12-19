c750e380ddabde6876d5725301d639da6115d043
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-12-19 15:21:35 +0330

    Done with not

--- lib/app/screens/home/tabs/not.dart
+++ lib/app/screens/home/tabs/not.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:whatsdone/app/services/database.dart';
 
 class NotDone extends StatefulWidget {
   @override
@@ -6,78 +7,102 @@ class NotDone extends StatefulWidget {
 }
 
 class _NotDoneState extends State<NotDone> {
-  List<Map> x = [
-    {
-      'name': 'amir',
-      'date': 'amir',
-    },
-  ];
-
   @override
   Widget build(BuildContext context) {
-    return x.isEmpty
-        ? Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'Nothing added',
-                  style: TextStyle(
-                    fontSize: 35,
-                    color: Colors.deepPurple,
-                  ),
-                ),
-                Text(
-                  'List is empty',
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.grey,
-                  ),
-                ),
-              ],
+    return FutureBuilder<List<Tasks>>(
+      future: DatabaseHelper.instance.getTasks('home'),
+      builder: (BuildContext context, AsyncSnapshot<List<Tasks>> snapshot) {
+        if (!snapshot.hasData) {
+          return Center(
+            child: CircularProgressIndicator(
+              color: Colors.deepPurple,
             ),
-          )
-        : ListView.separated(
-            separatorBuilder: (context, index) {
-              return Divider(color: Colors.deepPurple);
-            },
-            itemCount: x.length,
-            itemBuilder: (context, index) {
-              return Dismissible(
-                key: UniqueKey(),
-                child: ListTile(
-                  subtitle: Text(x[index]['date']),
-                  title: Text(
-                    x[index]['name'],
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.deepPurple,
-                    ),
-                  ),
-                ),
-                onDismissed: (direction) {
-                  if (direction == DismissDirection.endToStart) {
-                    print('Go to trash');
-                  } else {
-                    print('Go to done');
-                  }
-                },
-                background: Container(
-                  color: Colors.green,
-                  child: Icon(
-                    Icons.check,
-                    color: Colors.white,
-                  ),
-                ),
-                secondaryBackground: Container(
-                  color: Colors.red,
-                  child: Icon(
-                    Icons.delete,
-                    color: Colors.white,
-                  ),
-                ),
-              );
-            },
           );
+        }
+        return snapshot.data.isEmpty
+            ? Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      'Nothing added',
+                      style: TextStyle(
+                        fontSize: 35,
+                        color: Colors.deepPurple,
+                      ),
+                    ),
+                    Text(
+                      'List is empty',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            : ListView(
+                children: snapshot.data.map((task) {
+                  return Dismissible(
+                    key: UniqueKey(),
+                    child: ListTile(
+                      title: Text(
+                        task.name,
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.deepPurple,
+                        ),
+                      ),
+                      subtitle: Text(
+                        task.date,
+                        style: TextStyle(
+                          fontSize: 15,
+                        ),
+                      ),
+                    ),
+                    onDismissed: (direction) {
+                      if (direction == DismissDirection.endToStart) {
+                        setState(() {
+                          DatabaseHelper.instance.update(
+                            Tasks(
+                              id: task.id,
+                              date: task.date,
+                              name: task.name,
+                              status: 'trash',
+                            ),
+                          );
+                        });
+                      } else {
+                        setState(() {
+                          DatabaseHelper.instance.update(
+                            Tasks(
+                              id: task.id,
+                              date: task.date,
+                              name: task.name,
+                              status: 'done',
+                            ),
+                          );
+                        });
+                      }
+                    },
+                    background: Container(
+                      color: Colors.green,
+                      child: Icon(
+                        Icons.check,
+                        color: Colors.white,
+                      ),
+                    ),
+                    secondaryBackground: Container(
+                      color: Colors.red,
+                      child: Icon(
+                        Icons.delete,
+                        color: Colors.white,
+                      ),
+                    ),
+                  );
+                }).toList(),
+              );
+      },
+    );
   }
 }
