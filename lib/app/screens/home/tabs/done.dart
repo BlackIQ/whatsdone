bd8dbb7cde2c971f10fdee7a1a9b165f4a200017
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-12-19 15:25:06 +0330

    Done with done

--- lib/app/screens/home/tabs/done.dart
+++ lib/app/screens/home/tabs/done.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:whatsdone/app/services/database.dart';
 
 class DoneTasks extends StatefulWidget {
   @override
@@ -7,78 +7,102 @@ class DoneTasks extends StatefulWidget {
 }
 
 class _DoneTasksState extends State<DoneTasks> {
-  List<Map> x = [
-    {
-      'name': 'amir',
-      'date': 'amir',
-    },
-  ];
-
   @override
   Widget build(BuildContext context) {
-    return x.isEmpty
-        ? Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'You did everything',
-                  style: TextStyle(
-                    fontSize: 35,
-                    color: Colors.green,
-                  ),
-                ),
-                Text(
-                  'Congratulations',
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.deepPurple,
-                  ),
-                ),
-              ],
+    return FutureBuilder<List<Tasks>>(
+      future: DatabaseHelper.instance.getTasks('home'),
+      builder: (BuildContext context, AsyncSnapshot<List<Tasks>> snapshot) {
+        if (!snapshot.hasData) {
+          return Center(
+            child: CircularProgressIndicator(
+              color: Colors.deepPurple,
             ),
-          )
-        : ListView.separated(
-            separatorBuilder: (context, index) {
-              return Divider(color: Colors.deepPurple);
-            },
-            itemCount: x.length,
-            itemBuilder: (context, index) {
-              return Dismissible(
-                key: UniqueKey(),
-                child: ListTile(
-                  subtitle: Text(x[index]['date']),
-                  title: Text(
-                    x[index]['name'],
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.deepPurple,
-                    ),
-                  ),
-                ),
-                onDismissed: (direction) {
-                  if (direction == DismissDirection.endToStart) {
-                    print('Go to trash');
-                  } else {
-                    print('Go to not');
-                  }
-                },
-                background: Container(
-                  color: Colors.blueAccent,
-                  child: Icon(
-                    Icons.timer,
-                    color: Colors.white,
-                  ),
-                ),
-                secondaryBackground: Container(
-                  color: Colors.red,
-                  child: Icon(
-                    Icons.delete,
-                    color: Colors.white,
-                  ),
-                ),
-              );
-            },
           );
+        }
+        return snapshot.data.isEmpty
+            ? Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      'You did everything',
+                      style: TextStyle(
+                        fontSize: 35,
+                        color: Colors.green,
+                      ),
+                    ),
+                    Text(
+                      'Congratulations',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.deepPurple,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            : ListView(
+                children: snapshot.data.map((task) {
+                  return Dismissible(
+                    key: UniqueKey(),
+                    child: ListTile(
+                      title: Text(
+                        task.name,
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.deepPurple,
+                        ),
+                      ),
+                      subtitle: Text(
+                        task.date,
+                        style: TextStyle(
+                          fontSize: 15,
+                        ),
+                      ),
+                    ),
+                    onDismissed: (direction) {
+                      if (direction == DismissDirection.endToStart) {
+                        setState(() {
+                          DatabaseHelper.instance.update(
+                            Tasks(
+                              id: task.id,
+                              date: task.date,
+                              name: task.name,
+                              status: 'trash',
+                            ),
+                          );
+                        });
+                      } else {
+                        setState(() {
+                          DatabaseHelper.instance.update(
+                            Tasks(
+                              id: task.id,
+                              date: task.date,
+                              name: task.name,
+                              status: 'home',
+                            ),
+                          );
+                        });
+                      }
+                    },
+                    background: Container(
+                      color: Colors.blueAccent,
+                      child: Icon(
+                        Icons.timer,
+                        color: Colors.white,
+                      ),
+                    ),
+                    secondaryBackground: Container(
+                      color: Colors.red,
+                      child: Icon(
+                        Icons.delete,
+                        color: Colors.white,
+                      ),
+                    ),
+                  );
+                }).toList(),
+              );
+      },
+    );
   }
 }
