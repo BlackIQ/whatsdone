e793f2f08e15859da0ef5228e561b2fe245abed0
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-12-20 09:40:56 +0330

    Migrate new DatabaseService with not done page

--- lib/app/screens/home/tabs/not.dart
+++ lib/app/screens/home/tabs/not.dart
@@ -10,9 +10,9 @@ class NotDone extends StatefulWidget {
 class _NotDoneState extends State<NotDone> {
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<List<Tasks>>(
-      future: DatabaseHelper.instance.getTasks('home'),
-      builder: (BuildContext context, AsyncSnapshot<List<Tasks>> snapshot) {
+    return FutureBuilder<List<Task>>(
+      future: DatabaseService.instance.selectTasks('home'),
+      builder: (BuildContext context, AsyncSnapshot<List<Task>> snapshot) {
         if (!snapshot.hasData) {
           return Center(
             child: CircularProgressIndicator(
@@ -64,8 +64,9 @@ class _NotDoneState extends State<NotDone> {
                     onDismissed: (direction) {
                       if (direction == DismissDirection.endToStart) {
                         setState(() {
-                          DatabaseHelper.instance.update(
-                            Tasks(
+                          DatabaseService.instance.updateTask(
+                            Task(
+                              row: task.row,
                               id: task.id,
                               date: task.date,
                               name: task.name,
@@ -75,8 +76,9 @@ class _NotDoneState extends State<NotDone> {
                         });
                       } else {
                         setState(() {
-                          DatabaseHelper.instance.update(
-                            Tasks(
+                          DatabaseService.instance.updateTask(
+                            Task(
+                              row: task.row,
                               id: task.id,
                               date: task.date,
                               name: task.name,
