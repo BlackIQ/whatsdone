5d14854f7bfc41ebbdc040b1db7330ad3598c55c
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-12-30 07:51:08 +0330

    Working on update

--- lib/app/screens/home/routes/update.dart
+++ lib/app/screens/home/routes/update.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:whatsdone/app/models/task.dart';
+import 'package:whatsdone/app/services/database.dart';
+import 'package:whatsdone/app/widgets/toast.dart';
 
-class UpdateTask extends StatelessWidget {
+class UpdateTask extends StatefulWidget {
   UpdateTask({
     this.task,
   });
@@ -8,15 +12,83 @@ class UpdateTask extends StatelessWidget {
   final task;
 
   @override
+  State<UpdateTask> createState() => _UpdateTaskState();
+}
+
+class _UpdateTaskState extends State<UpdateTask> {
+  @override
   Widget build(BuildContext context) {
+    TextEditingController _name = TextEditingController();
+    TextEditingController _note = TextEditingController();
+
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         elevation: 0,
-        title: Text(task.name),
+        leading: FlatButton(
+          child: Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+          onPressed: () {
+            Navigator.of(context).pop();
+            Navigator.of(context).pop();
+            Navigator.of(context).pop();
+          },
+        ),
+        title: Text('Update task'),
         actions: [
           FlatButton(
-            onPressed: () {},
+            onPressed: () {
+              if (_name.text == '' && _note.text == '') {
+                return showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                    title: Text(
+                      'Null changes!?',
+                      style: TextStyle(
+                        color: Colors.deepPurple,
+                      ),
+                    ),
+                    content: Text(
+                      'You filled none of the fields. So app is not going to replace data with null items.',
+                    ),
+                    actions: [
+                      TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: Text(
+                          'Ok',
+                          style: TextStyle(
+                            color: Colors.deepPurple,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  barrierDismissible: false,
+                );
+              } else {
+                print(_name.text);
+                print(_note.text);
+                setState(() {
+                  DatabaseService.instance.update(
+                    Task(
+                      id: widget.task.id,
+                      date: widget.task.date,
+                      status: widget.task.status,
+                      name:
+                          _name.text.isNotEmpty ? _name.text : widget.task.name,
+                      note:
+                          _note.text.isNotEmpty ? _note.text : widget.task.note,
+                      row: widget.task.row,
+                    ),
+                  );
+                });
+                sendToast('Task data is changed', true);
+              }
+            },
             child: Text(
               'Update',
               style: TextStyle(
@@ -32,69 +104,46 @@ class UpdateTask extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              task.name,
-              style: TextStyle(
-                fontSize: 20,
-                color: Colors.deepPurple,
+            TextField(
+              decoration: InputDecoration(
+                hintText: widget.task.name,
+                labelText: 'Name',
               ),
+              controller: _name,
+              obscureText: false,
             ),
-            Divider(color: Colors.deepPurple),
+            SizedBox(height: 5),
+            TextField(
+              decoration: InputDecoration(
+                hintText: widget.task.note,
+                labelText: 'Note',
+              ),
+              controller: _note,
+              obscureText: false,
+            ),
+            SizedBox(height: 50),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                FaIcon(
+                  FontAwesomeIcons.exclamationTriangle,
+                  color: Colors.red,
+                ),
+                SizedBox(width: 20),
+                Text(
+                  'Note',
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 5),
             Text(
-              task.note,
-            ),
-            SizedBox(height: 20),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Task status stage',
-                  style: TextStyle(
-                    color: Colors.deepPurple,
-                  ),
-                ),
-                Text(
-                  task.status,
-                  style: TextStyle(
-                    color: task.status == 'home'
-                        ? Colors.blue
-                        : task.status == 'trash'
-                            ? Colors.red
-                            : Colors.green,
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Date created',
-                  style: TextStyle(
-                    color: Colors.deepPurple,
-                  ),
-                ),
-                Text(
-                  task.date,
-                ),
-              ],
-            ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Task ID',
-                  style: TextStyle(
-                    color: Colors.deepPurple,
-                  ),
-                ),
-                Text(
-                  task.id.toString(),
-                ),
-              ],
+              'You only can change name and note here. It is impossible to change date and id.',
+              style: TextStyle(color: Colors.red),
             ),
           ],
         ),
