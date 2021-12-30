e4c1ed90468152da6a82076bad1da899b9dee5d1
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-12-30 07:01:52 +0330

    Create context menu

--- lib/app/screens/home/tabs/not.dart
+++ lib/app/screens/home/tabs/not.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:whatsdone/app/screens/home/routes/task.dart';
+import 'package:whatsdone/app/screens/home/routes/update.dart';
 import 'package:whatsdone/app/services/database.dart';
 import 'package:whatsdone/app/models/task.dart';
+import 'package:whatsdone/app/widgets/buttons/menu_button.dart';
 import 'package:whatsdone/app/widgets/toast.dart';
 
 class NotDone extends StatefulWidget {
@@ -50,13 +53,41 @@ class _NotDoneState extends State<NotDone> {
                     return Dismissible(
                       key: UniqueKey(),
                       child: ListTile(
-                        onLongPress: () {
-                          Navigator.of(context).push(
-                            MaterialPageRoute(
-                              builder: (context) => OpenTask(task: task),
+                        onLongPress: () => showDialog(
+                          context: context,
+                          builder: (context) => AlertDialog(
+                            content: SingleChildScrollView(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.stretch,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  MenuButton(
+                                    text: 'Open task',
+                                    icon: FontAwesomeIcons.search,
+                                    click: () {
+                                      Navigator.of(context).push(
+                                        MaterialPageRoute(
+                                          builder: (context) => OpenTask(task: task),
+                                        ),
+                                      );
+                                    },
+                                  ),
+                                  MenuButton(
+                                    text: 'Update task',
+                                    icon: FontAwesomeIcons.edit,
+                                    click: () {
+                                      Navigator.of(context).push(
+                                        MaterialPageRoute(
+                                          builder: (context) => UpdateTask(task: task),
+                                        ),
+                                      );
+                                    },
+                                  ),
+                                ],
+                              ),
                             ),
-                          );
-                        },
+                          ),
+                        ),
                         title: Text(
                           task.name,
                           style: TextStyle(
