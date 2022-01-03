3039e0a6385e1cfae9fb03b4b8e787e59dc7a6fe
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-01-03 08:32:40 +0330

    Add Anon Front

--- lib/app/screens/authentication/authentication.dart
+++ lib/app/screens/authentication/authentication.dart
@@ -134,6 +134,95 @@ class _AuthenticationState extends State<Authentication> {
                 ),
               ),
             ),
+            SizedBox(height: 10),
+            Container(
+              height: 50,
+              child: ElevatedButton(
+                onPressed: () async {
+                  try {
+                    final result = await InternetAddress.lookup('google.com');
+                    if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
+                      dynamic result = await _auth.anonLogin();
+                      if (result.runtimeType == List) {
+                        return showDialog(
+                          context: context,
+                          builder: (context) => AlertDialog(
+                            title: Text(
+                              'Something went wrong',
+                              style: TextStyle(
+                                color: Colors.deepPurple,
+                              ),
+                            ),
+                            content: Text(
+                              result[1],
+                            ),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: Text(
+                                  'Ok',
+                                  style: TextStyle(
+                                    color: Colors.deepPurple,
+                                  ),
+                                ),
+                              ),
+                            ],
+                            elevation: 0,
+                          ),
+                          barrierDismissible: false,
+                        );
+                      }
+                    }
+                  } catch (e) {
+                    return showDialog(
+                      context: context,
+                      builder: (context) => AlertDialog(
+                        title: Text(
+                          'Something went wrong',
+                          style: TextStyle(
+                            color: Colors.deepPurple,
+                          ),
+                        ),
+                        content: Text(
+                          'It seems there is no internet connection.',
+                        ),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: Text(
+                              'Ok',
+                              style: TextStyle(
+                                color: Colors.deepPurple,
+                              ),
+                            ),
+                          ),
+                        ],
+                        elevation: 0,
+                      ),
+                      barrierDismissible: false,
+                    );
+                  }
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    FaIcon(
+                      FontAwesomeIcons.userSecret,
+                    ),
+                    Text(
+                      'Sign in as guest',
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
             SizedBox(height: 20),
             Text(
               'You should sign in to continue to the app',
