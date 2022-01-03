97f65bf83326f57d64900e216c9a43a648ebf655
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-01-03 08:09:57 +0330

    Create bottom modal

---
+++ lib/app/widgets/modal/modalbottom.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void modalBottom(context, name, note) {
+  showModalBottomSheet(
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.only(
+        topLeft: Radius.circular(15),
+        topRight: Radius.circular(15),
+      ),
+    ),
+    context: context,
+    builder: (context) => SingleChildScrollView(
+      padding: EdgeInsets.all(20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Text(
+            name,
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+              color: Colors.deepPurple,
+            ),
+          ),
+          SizedBox(height: 20),
+          Text(
+            'Note',
+            style: TextStyle(
+              fontSize: 12,
+              color: Colors.grey,
+            ),
+          ),
+          Text(
+            note,
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
+}
