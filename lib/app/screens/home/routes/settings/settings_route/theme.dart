cdf1e9b6bbe4a8054a4af2f35132ea572e87ef09
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-01-06 22:43:33 +0330

    Create the sample for theme card

--- lib/app/screens/home/routes/settings/settings_route/theme.dart
+++ lib/app/screens/home/routes/settings/settings_route/theme.dart
@@ -4,6 +4,9 @@ class ThemeSettings extends StatelessWidget {
   List<Map> items = [
     {
       'title': 'Default theme',
+      'foreground-color': 'Deep Purple',
+      'foreground-class': Colors.deepPurple,
+      'background-color': 'White',
       'details': 'Deep-purple text + White background',
       'leading': '💎',
     },
@@ -27,43 +30,46 @@ class ThemeSettings extends StatelessWidget {
   }
 
   Widget body() {
-    return SingleChildScrollView(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Expanded(
-            child: ListView.builder(
-              shrinkWrap: true,
-              // scrollDirection: Axis.horizontal,
-              padding: EdgeInsets.all(20),
-              itemCount: items.length,
-              itemBuilder: (context, index) => Card(
-                margin: EdgeInsets.all(15),
-                child: Padding(
-                  padding: EdgeInsets.all(15),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Text('Ho'),
-                      SizedBox(height: 20),
-                      Container(
-                        height: 40,
-                        child: ElevatedButton(
-                          onPressed: () {},
-                          child: Text('Activate ${items[index]['leading']}'),
-                        ),
-                      ),
-                    ],
-                  ),
+    return ListView.builder(
+      // scrollDirection: Axis.horizontal,
+      padding: EdgeInsets.all(20),
+      itemCount: items.length,
+      itemBuilder: (context, index) => Card(
+        margin: EdgeInsets.all(15),
+        child: Padding(
+          padding: EdgeInsets.all(15),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text('Theme name', style: TextStyle(fontWeight: FontWeight.bold, color: items[index]['foreground-class'])),
+              Text(items[index]['title'], style: TextStyle(color: items[index]['foreground-class'])),
+              SizedBox(height: 10),
+              Text('Foreground Color', style: TextStyle(fontWeight: FontWeight.bold, color: items[index]['foreground-class'])),
+              Text(items[index]['foreground-color'], style: TextStyle(color: items[index]['foreground-class'])),
+              SizedBox(height: 10),
+              Text('Background Color', style: TextStyle(fontWeight: FontWeight.bold, color: items[index]['foreground-class'])),
+              Text(items[index]['background-color'], style: TextStyle(color: items[index]['foreground-class'])),
+              SizedBox(height: 25),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('Theme Logo', style: TextStyle(fontWeight: FontWeight.bold, color: items[index]['foreground-class'])),
+                  Text(items[index]['leading']),
+                ],
+              ),
+              SizedBox(height: 15),
+              Container(
+                height: 40,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Activate ${items[index]['leading']}'),
                 ),
               ),
-            ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
